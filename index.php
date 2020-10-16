4b7b8eaa3cdff058aad5f193ec5cd88552c2c2cc
Priet0 <72607064+Priet0@users.noreply.github.com>
2020-10-16 11:13:34 +0200

    vinvinvin

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 	</footer>
 </body>
 </html>
+=======
 <?php
 // à utiliser avec front-page.php
 //wp_redirect(site_url());
@@ -23,15 +24,25 @@
 ?>
 <?php get_header(); ?>
 
+<!--pull anthony-->
+
 <section id='content'>
+
   <aside>
-    <fieldset id="fieldsetsidebar">
+    <div>
+
+  <legend>index.php: &lt;aside&gt; + <code>get_sidebar()</code></legend>
+
   <?php get_sidebar(); ?>
+  
+    </div>
   </aside>
 
   <fieldset id="fieldsetcontent">
+    <legend>index.php: contenu avec the_loop <code>while ( have_posts() )</code></legend>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <fieldset class="post">
+    <legend>Article <code>the_post()</code> :</legend>
 
    	<!-- Display the Title as a link to the Post's permalink. -->
 
@@ -47,6 +58,7 @@
 
    	<div class="entry">
       <fieldset>
+       <legend>Contenu de l'article <code>the_content()</code></legend>
    		<?php the_content(); ?>
     </fieldset>
    	</div>
@@ -59,6 +71,7 @@
   </p>
 
     <fieldset style="margin-left:10%;">
+      <legend>Commentaires : <code>comments_template()</code></legend>
 <?php
     if ( comments_open() || get_comments_number() ) :
               comments_template();
@@ -73,3 +86,4 @@
 </section>
 
 <?php get_footer(); ?>
+>>>>>>> 413fdb0976d0e88f82f2aebadeb47ecc3784e457
--- page.php
+++ page.php
@@ -39,7 +39,7 @@
 
    	<!-- Display the Title as a link to the Post's permalink. -->
 
-   	<h2>Titre de l'article <code>the_title()</code>: <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+   	<h2><code>the_title()</code> <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
 
    	<!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
