874bff00ca35fe4476773b29496bb677ca1ee962
STVGecko <steeven.lombardi@gmail.com>
2020-10-20 13:38:35 +0200

    virer code dans page.php

--- page.php
+++ page.php
@@ -14,7 +14,7 @@
 	</footer>
 </body>
 </html>
- 
+
 <?php
 // à utiliser avec front-page.php
 //wp_redirect(site_url());
@@ -32,14 +32,14 @@
   </aside>
 
   <fieldset id="fieldsetcontent">
-    <legend>index.php: contenu avec the_loop <code>while ( have_posts() )</code></legend>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <fieldset class="post">
-    <legend>Article <code>the_post()</code> :</legend>
+
 
    	<!-- Display the Title as a link to the Post's permalink. -->
 
-   	<h2><code>the_title()</code> <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+   	<h2> <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
 
    	<!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
@@ -51,7 +51,6 @@
 
    	<div class="entry">
       <fieldset>
-       <legend>Contenu de l'article <code>the_content()</code></legend>
    		<?php the_content(); ?>
     </fieldset>
    	</div>
@@ -64,7 +63,6 @@
   </p>
 
     <fieldset style="margin-left:10%;">
-      <legend>Commentaires : <code>comments_template()</code></legend>
 <?php
     if ( comments_open() || get_comments_number() ) :
               comments_template();
