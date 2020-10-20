e155f25d90646d3b670cb69f12bd5de352effbe2
AnthonyNhlg <antho.nehlig@gmail.com>
2020-10-20 13:41:18 +0200

    suppresion legende code du single

--- single.php
+++ single.php
@@ -9,7 +9,7 @@
 	<meta charset="utf-8">
 </head>
 <body>
-	<header> 
+	<header>
 	</header>
 	<footer>
 
@@ -24,7 +24,6 @@
 ?>
 <?php get_header(); ?>
 
-<!--pull anthony-->
 
 <section id='content'>
   <aside>
@@ -37,36 +36,36 @@
     <legend>index.php: contenu avec the_loop <code>while ( have_posts() )</code></legend>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <fieldset class="post">
-    <legend>Article <code>the_post()</code> :</legend>
+    <p>Ceci est l'article :</p>
 
    	<!-- Display the Title as a link to the Post's permalink. -->
 
-   	<h2>Titre de l'article <code>the_title()</code>: <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+   	<h2>Titre de l'article: <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
 
    	<!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
 
-   	<small>Date et auteur <code>the_time('j F Y à G:i')</code> : <?php the_time('j F Y à G:i'); ?> par <code>the_author_posts_link()</code> : <?php the_author_posts_link(); ?></small>
+   	<small>Date et auteur : <?php the_time('j F Y à G:i'); ?> par : <?php the_author_posts_link(); ?></small>
 
 
    	<!-- Display the Post's content in a div box. -->
 
    	<div class="entry">
       <fieldset>
-       <legend>Contenu de l'article <code>the_content()</code></legend>
+       <p>Ceci est le contenu de l'article</p>
    		<?php the_content(); ?>
     </fieldset>
    	</div>
 
     <p style="font-size:.9em;">
       <!-- Display the number of comments. -->
-      Cet article a <code>comments_number(...)</code><a href="<?php the_permalink(); ?>#comments"><?php comments_number( 'aucun commentaire', 'un commentaire', '% commentaires' ); ?></a>.
+      Cet article a <a href="<?php the_permalink(); ?>#comments"><?php comments_number( 'aucun commentaire', 'un commentaire', '% commentaires' ); ?></a>.
    	<!-- Display a comma separated list of the Post's Categories. -->
-   	<span class="postmetadata">Posté dans <code>the_category(', ')</code><?php the_category( ', ' ); ?></span>
+   	<span class="postmetadata">Posté dans <?php the_category( ', ' ); ?></span>
   </p>
 
     <fieldset style="margin-left:10%;">
-      <legend>Commentaires : <code>comments_template()</code></legend>
+      <p>Ceci sont les commentaires : ></p>
 <?php
     if ( comments_open() || get_comments_number() ) :
               comments_template();
