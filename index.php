5993c3c506e390e3c64ecbbd5d2d2add15d58146
lucas74200 <46594332+lucas74200@users.noreply.github.com>
2020-10-20 13:18:01 +0200

    okay

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 	<footer>
 
 	</footer>
-</body> 
+</body>
 </html>
 
 <?php
@@ -33,7 +33,7 @@
   <aside>
     <div>
 
-  <legend>index.php: &lt;aside&gt; + <code>get_sidebar()</code></legend>
+
 
   <?php get_sidebar(); ?>
 
@@ -41,10 +41,10 @@
   </aside>
 
   <fieldset id="fieldsetcontent">
-    <legend>index.php: contenu avec the_loop <code>while ( have_posts() )</code></legend>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
   <fieldset class="post">
-    <legend>Article <code>the_post()</code> :</legend>
+
 
    	<!-- Display the Title as a link to the Post's permalink. -->
 
@@ -60,7 +60,7 @@
 
    	<div class="entry">
       <fieldset>
-       <legend>Contenu de l'article <code>the_content()</code></legend>
+
    		<?php the_content(); ?>
     </fieldset>
    	</div>
@@ -73,7 +73,7 @@
   </p>
 
     <fieldset style="margin-left:10%;">
-      <legend>Commentaires : <code>comments_template()</code></legend>
+      
 <?php
     if ( comments_open() || get_comments_number() ) :
               comments_template();
