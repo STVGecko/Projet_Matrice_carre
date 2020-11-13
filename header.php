025647b5c7944becbc1fb580cc61e2a5c0b568a5
STVGecko <steeven.lombardi@gmail.com>
2020-11-13 13:43:02 +0100

    sssc

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
         <title><?php bloginfo('name');?> - <?php bloginfo('description');?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-      <!--  <link rel="stylesheet" type="text/css" href="<?php// echo get_template_directory_uri();?>/css/site.css"/> ceci est la page pour lier le css, ne pas enlever-->
+      <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/site.css"/> <!--ceci est la page pour lier le css, ne pas enlever-->
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <?php wp_head(); ?>
     </head>
