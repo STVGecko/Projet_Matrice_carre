33e39bf6f52d9bd349b5e28d920b63f3cee7a189
STVGecko <steeven.lombardi@gmail.com>
2020-11-13 13:13:21 +0100

    correction

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <title><?php bloginfo('name');?> - <?php bloginfo('description');?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/site.css"></style>
+        <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri();?>/css/site.css"/>
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <?php wp_head(); ?>
     </head>
