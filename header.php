8562964daf4d2b54df7b710488ca80d9a2338b5e
STVGecko <steeven.lombardi@gmail.com>
2020-11-13 13:42:09 +0100

    no cache

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta http-equiv="cache-control" content="no-cache"/>
         <title><?php bloginfo('name');?> - <?php bloginfo('description');?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
